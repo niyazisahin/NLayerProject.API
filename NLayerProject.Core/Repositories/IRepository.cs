00f448b6e3032c8d717d3fd482cac93721be783c
niyazi şahin <niyazisahin3800@gmail.com>
2021-04-12 01:01:13 +0300

    Add project files.

---
+++ NLayerProject.Core/Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(int Id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> SingeOrDefault(Expression<Func<TEntity, bool>> predicate);
+        Task Add(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+        TEntity Update(TEntity entity);
+    }
+}
